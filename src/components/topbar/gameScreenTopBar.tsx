57471f5c7eedcb6bb715ea06b72a6abe6f5ebb25
kym8821 <louie9798@naver.com>
2024-07-17 23:27:18 +0900

    [feat] add style and health bar on topbar

--- src/components/topbar/gameScreenTopBar.tsx
+++ src/components/topbar/gameScreenTopBar.tsx
@@ -1,22 +1,70 @@
 import { Resource } from "../../util/resource";
 import style from "../../assets/css/gameScreen.module.css";
+import { MapManager } from "../../util/map/mapManager";
+import mapElementInfo from "../../util/map/mapElementInfo";
+import systemImages from "../../assets/images/system/systemImages";
 
 interface GameScreenTopBarProps {
   resource: Resource;
+  mapManager: React.MutableRefObject<MapManager>;
 }
 
-const GameScreenTopBar = ({ resource }: GameScreenTopBarProps) => {
+const cssStyle: Record<string, React.CSSProperties> = {
+  healthBarContainer: {
+    position: "relative",
+    display: "flex",
+    width: "15vw",
+    height: "2vw",
+    backgroundColor: "#fff",
+    overflow: "hidden",
+  },
+  healthSection: {
+    position: "absolute",
+    backgroundColor: "red",
+    width: "100%",
+    height: "100%",
+    top: 0,
+    transition: "right 0.5s ease",
+  },
+  healthState: {
+    position: "absolute",
+    top: 0,
+  },
+  separatorImage: {
+    position: "absolute",
+    width: "0.5vw",
+    height: "100%",
+    top: 0,
+    transition: "left 0.5s ease",
+  },
+  resourceContainer: { display: "flex", columnGap: "2vw", height: "2vw" },
+  resourceStyle: { display: "flex", alignItems: "center", columnGap: "0.5vw" },
+  resourceImage: {
+    width: "1vw",
+    aspectRatio: 1,
+  },
+};
+
+const GameScreenTopBar = ({ resource, mapManager }: GameScreenTopBarProps) => {
   return (
-    <div className={style.gameScreenTopBar}>
-      <div>
-        energy : {resource.energy} ( {resource.energyOutput}/sec )
+    <div className={style.gameScreenTopBar} style={{ height: `${mapManager.current.blockSize}px` }}>
+      <div style={cssStyle.resourceContainer}>
+        <div style={cssStyle.resourceStyle}>
+          <img src={systemImages.pointerLeft} style={{ ...cssStyle.resourceImage }} />
+          <p>
+            {resource.energy} ( {resource.energyOutput}/sec )
+          </p>
+        </div>
+        <div style={cssStyle.resourceStyle}>
+          <img src={systemImages.pointerLeft} style={{ ...cssStyle.resourceImage }} />
+          <p>
+            {resource.evolveFactor} ( {resource.evolveFactorOutput}/sec )
+          </p>
+        </div>
       </div>
-      <div>
-        evolveFactor : {resource.evolveFactor} ( {resource.evolveFactorOutput}/sec )
-      </div>
-      <div style={{ display: "flex" }}>
-        <div style={{ backgroundColor: "red", width: `${resource.health}%` }}>{resource.health}</div>
-        <div style={{ backgroundColor: "white", widows: `${100 - resource.health}%` }}></div>
+      <div style={cssStyle.healthBarContainer}>
+        <div style={{ ...cssStyle.healthSection, right: `${100 - resource.health}%` }} />
+        <div style={cssStyle.healthState}>{resource.health}</div>
       </div>
     </div>
   );
